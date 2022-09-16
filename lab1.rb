623144576c219b84f610e93b5f05bc1e8a9645a2
vanthienpham <vanthien.dev@gmail.com>
2022-09-16 19:06:22 +0700

    lab2

--- lab1.rb
+++ lab1.rb
@@ -13,3 +13,20 @@ elsif numB > numA
 else puts "Hai số bằng nhau";
 end
 
+books = {}
+books["The deep end"]  = :tot
+books["Living colors"] = :xe
+
+puts books
+
+puts books.length
+
+sum = 0
+for i in 0..100
+    if i % 5 == 0 
+        puts i ;
+        sum += i;
+    end
+end
+puts "total : #{sum}";
+
